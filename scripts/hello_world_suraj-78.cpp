cb051a7feba79eba536d4f355efae2039b17e3b1
suraj-78 <surajpandit591@gmail.com>
2023-10-17 22:40:27 +0530

    Create hello_world_suraj-78.cpp

---
+++ scripts/hello_world_suraj-78.cpp
@@ -0,0 +1,15 @@
+// LANGUAGE: C++
+// ENV: g++
+// AUTHOR: Suraj Pandit
+// GITHUB: https://github.com/suraj-78
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cout<<"Aman !!"<<endl;
+    }
+}
